1911ac1279b13999288ed49cf9cfde89937c5012
Mathieu <mathieu.eylenbosch@gmail.com>
2026-01-13 19:02:31 +0100

    Removed unused class

--- src/app/entities/User.ts
+++ src/app/entities/User.ts
@@ -3,9 +3,3 @@ export class User {
     public Name: string = null!
     public Roles: string[] = null!
 }
-
-export class SimpleUser {
-    public Id: number = null!
-    public Name: string = null!
-    public isIntern = false
-}
